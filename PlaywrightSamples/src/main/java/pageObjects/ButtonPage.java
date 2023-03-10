bd455fc0ae6d6220947d52bfe2660eb2d36c3dde
sasidaran.kumarasamy <freyjasasi@gmail.com>
2023-03-10 23:15:59 +0530

    button tests added

---
+++ PlaywrightSamples/src/main/java/pageObjects/ButtonPage.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+
+import lombok.Getter;
+import testBase.TestBase;
+
+@Getter
+public class ButtonPage extends TestBase {
+
+	private static ButtonPage buttonPage;
+
+	public static ButtonPage getInstance() {
+		if (buttonPage == null) {
+			buttonPage = new ButtonPage();
+		}
+		return buttonPage;
+	}
+
+	private Locator homePageButtonLocator = page.locator("id=home");
+	private Locator findLocationButtonLocator = page.locator("id=position");
+	private Locator findColorButtonLocator = page.locator("id=color");
+	private Locator findHeightButtonLocator = page.locator("id=color");
+	private Locator disableButtonLocator = page.getByRole(AriaRole.BUTTON,
+			new Page.GetByRoleOptions().setName("Disabled"));
+	private Locator longPressButtonLocator = page.locator("h2");
+}
---
+++ PlaywrightSamples/src/test/java/sampleTests/ButtonTest.java
@@ -0,0 +1,35 @@
+package sampleTests;
+
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Locator.ClickOptions;
+
+import pageObjects.ButtonPage;
+import testBase.TestBase;
+
+public class ButtonTest extends TestBase {
+
+	ButtonTest() {
+		super();
+	}
+
+	@Test(groups = { "button" })
+	public void buttonTest01() {
+		ButtonPage buttonPage = ButtonPage.getInstance();
+		page.navigate(prop.getProperty("buttonPage"));
+
+		System.out.println("Before clicking home page: " + page.title());
+		buttonPage.getHomePageButtonLocator().click();
+		System.out.println("After clicking home page buttion: " + page.title());
+		page.goBack();
+		System.out.println("Again in button page: " + page.title());
+
+		System.out.println("is Disabled button: " + buttonPage.getDisableButtonLocator().isDisabled());
+
+		System.out.println("Before button hold: " + buttonPage.getLongPressButtonLocator().textContent());
+		buttonPage.getLongPressButtonLocator().click(new ClickOptions().setDelay(4000));
+		System.out.println("After button hold: " + buttonPage.getLongPressButtonLocator().textContent());
+
+	}
+
+}
